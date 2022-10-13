5f1643b53c5f676cb519292565cc55890dab4bce
moYdaDyr <113461028+moYdaDyr@users.noreply.github.com>
2022-10-13 12:03:55 +0500

    Initial commit

---
+++ lab/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+
+namespace lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
